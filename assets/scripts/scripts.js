854175b696606f1b8e5551c8c3b44e5bb9c2d298
mr-tajul-Islam <tajul.azazil@gmail.com>
2025-04-19 16:01:14 +0600

    ok

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,38 +1,53 @@
-{
-           "@context": "http://www.schema.org",
-           "@type": "ProfessionalService",
-           "name": "ThinkTalks",
-           "title": "ThinkTalks - Online Courses for Career Growth",
-           "url": "/",
-           "sameAs": [
-              ""
-           ],
-           "logo": "https://moksy.com/api/build/uploads/79378433/uploads/logo.png",
-           "priceRange": "$$$",
-           "image": "https://moksy.com/api/build/uploads/79378433/uploads/socialshare.png",
-           "description": "Advance your career with online courses from ThinkTalks. Our platform offers courses to help you grow professionally.",
-           "address": {
-              "@type": "PostalAddress",
-              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
-              "addressLocality": "London",
-              "addressRegion": "Surrey",
-              "postalCode": "CR0 2BX",
-              "addressCountry": "United Kingdom"
-           },
-           "email": "support@moksy.com",
-           "geo": {
-              "@type": "GeoCoordinates",
-              "latitude": "51.37683",
-              "longitude": "-0.09728"
-           },
-           
-           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
-           "telephone": "+44 0808 175 1749",
-           "aggregateRating": {
-                 "@type": "AggregateRating",
-                 "ratingValue": "",
-                 "ratingCount": ""
-               },
-          "datePublished": ""
-         }
-Value for custom_js_code
+(function () {
+	const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
+
+	//I'm adding this section so I don't have to keep updating this pen every year :-)
+	//remove this if you don't need it
+	let today = new Date(),
+		dd = String(today.getDate()).padStart(2, "0"),
+		mm = String(today.getMonth() + 1).padStart(2, "0"),
+		yyyy = today.getFullYear(),
+		nextYear = yyyy + 1,
+		dayMonth = "09/30/",
+		birthday = dayMonth + yyyy;
+
+	today = mm + "/" + dd + "/" + yyyy;
+	if (today > birthday) {
+		birthday = dayMonth + nextYear;
+	}
+	//end
+
+	const countDown = new Date(birthday).getTime(),
+		x = setInterval(function () {
+			const now = new Date().getTime(),
+				distance = countDown - now;
+
+			(document.getElementById("days").innerText = Math.floor(distance / day)),
+				(document.getElementById("hours").innerText = Math.floor(
+					(distance % day) / hour
+				)),
+				(document.getElementById("minutes").innerText = Math.floor(
+					(distance % hour) / minute
+				)),
+				(document.getElementById("seconds").innerText = Math.floor(
+					(distance % minute) / second
+				));
+
+			//do something later when date is reached
+			if (distance < 0) {
+				document.getElementById("headline").innerText = "It's my birthday!";
+				document.getElementById("countdown").style.display = "none";
+				document.getElementById("content").style.display = "block";
+				clearInterval(x);
+			}
+			//seconds
+		}, 0);
+})();
+
+
+
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logo-slider").appendChild(copy);
